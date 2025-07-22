b209eb83bd33851f97be142c411c5b1aa45d015e
Asheiou <31135263+Asheiou@users.noreply.github.com>
2025-07-22 23:47:49 +0100

    move

--- src/main/kotlin/xyz/aeolia/lib/command/user/PVPCommandExecutor.kt
+++ src/main/kotlin/xyz/aeolia/lib/command/user/PVPCommandExecutor.kt
@@ -1,4 +1,4 @@
-package xyz.aeolia.lib.command
+package xyz.aeolia.lib.command.user
 
 import org.bukkit.Bukkit
 import org.bukkit.command.Command
@@ -11,7 +11,7 @@ import xyz.aeolia.lib.menu.PVPMenu
 import xyz.aeolia.lib.sender.MessageSender
 import xyz.aeolia.lib.utils.Message
 
-class PVPCommandExecutor(plugin1: JavaPlugin) : CommandExecutor, PVPMenu(plugin1) {
+class PVPCommandExecutor(p: JavaPlugin) : CommandExecutor, PVPMenu(p) {
   override fun onCommand(
     sender: CommandSender,
     command: Command,
@@ -19,54 +19,48 @@ class PVPCommandExecutor(plugin1: JavaPlugin) : CommandExecutor, PVPMenu(plugin1
     args: Array<out String>
   ): Boolean {
     if (sender !is Player) {
-      MessageSender.sendMessage(sender, Message.Generic.NOT_PLAYER)
+      MessageSender.Companion.sendMessage(sender, Message.Generic.NOT_PLAYER)
       return true
     }
-    if (args.isEmpty()||(!sender.hasPermission("lib.pvp.manage"))) {
+    if (args.isEmpty() || (!sender.hasPermission("lib.pvp.manage"))) {
       inventory.open(sender)
       return true
     }
 
-    if(args[0] == "respawn") {
+    if (args[0] == "respawn") {
       if (args.size == 1) {
-        PVPListener.tpPlayerToArena(sender, plugin)
-        return true
-      } else {
-        var condition = false
-        Bukkit.getOnlinePlayers().forEach { player ->
-          if (player.name == args[1]) {
-            PVPListener.tpPlayerToArena(player, plugin)
-            condition = true
-          }
-        }
-        if (!condition) {
-          MessageSender.sendMessage(sender, Message.Player.NOT_FOUND)
-        }
+        PVPListener.Companion.tpPlayerToArena(sender, plugin)
         return true
       }
+
+      Bukkit.getOnlinePlayers().firstOrNull { it.name == args[1] }?.let {
+        PVPListener.Companion.tpPlayerToArena(it, plugin)
+      } ?: MessageSender.Companion.sendMessage(sender, Message.Player.NOT_FOUND)
+
+      return true
     }
     if (args[0] == "addspawn") {
       val location = sender.location
       if (sender.world.name != plugin.config.getString("pvp.world")) {
-        MessageSender.sendMessage(sender, "You can only run this command in the PVP world!")
+        MessageSender.Companion.sendMessage(sender, "You can only run this command in the PVP world!")
         return true
       }
       val spawnLocations = plugin.config.getList("pvp.spawn-locations") ?: run {
-        MessageSender.sendMessage(sender, Message.Error.CONFIG.format("pvp.spawn-locations"))
+        MessageSender.Companion.sendMessage(sender, Message.Error.CONFIG.format("pvp.spawn-locations"))
         return true
       }
       val locationMap = mutableMapOf<String, Double>()
-      locationMap["x"] = location.blockX.toDouble()+0.5 //Centre on block
+      locationMap["x"] = location.blockX.toDouble() + 0.5 //Centre on block
       locationMap["y"] = location.blockY.toDouble()
-      locationMap["z"] = location.blockZ.toDouble()+0.5
+      locationMap["z"] = location.blockZ.toDouble() + 0.5
       val newSpawnLocations = spawnLocations.plus(locationMap)
       plugin.config.set("pvp.spawn-locations", newSpawnLocations)
       plugin.saveConfig()
       plugin.reloadConfig()
-      MessageSender.sendMessage(sender, "Location added.")
+      MessageSender.Companion.sendMessage(sender, "Location added.")
       return true
     }
-    MessageSender.sendMessage(sender, Message.Generic.COMMAND_USAGE)
+    MessageSender.Companion.sendMessage(sender, Message.Generic.COMMAND_USAGE)
     return false
   }
 }
