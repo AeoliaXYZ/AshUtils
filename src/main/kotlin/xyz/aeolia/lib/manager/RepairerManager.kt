f7baa4606547ad4799b98b354ad39d7cf5143c50
Asheiou <31135263+Asheiou@users.noreply.github.com>
2025-08-01 18:59:52 +0100

    compact

--- src/main/kotlin/xyz/aeolia/lib/manager/RepairerManager.kt
+++ src/main/kotlin/xyz/aeolia/lib/manager/RepairerManager.kt
@@ -8,23 +8,18 @@ import xyz.aeolia.lib.data.Repairer
 class RepairerManager(
   val plugin: JavaPlugin,
   val player: Player,
-  val items: List<ItemStack>,
-  val chargePerItem : Boolean =  plugin.config.getBoolean("repair.charge-per-item", true),
-  val repairCost : Double = plugin.config.getDouble("repair.cost", 0.0)
+  items: List<ItemStack>,
+  val chargePerItem: Boolean = plugin.config.getBoolean("repair.charge-per-item", true),
+  val repairCost: Double = plugin.config.getDouble("repair.cost", 0.0)
 ) {
   val repairers: MutableList<Repairer> = mutableListOf()
 
   init {
-    for (item in items) {
-      repairers.add(Repairer(item, player, this))
-    }
+    items.forEach { repairers.add(Repairer(it, player, this)) }
   }
 
   val costToRepair by lazy {
     var runningCost = 0.0
-    repairers.forEach {
-      runningCost += it.costToRepair
-    }
-    runningCost
+    repairers.forEach { runningCost += it.costToRepair }.also { runningCost }
   }
 }
