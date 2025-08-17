d18532dfe16c227af1ecb14ac03d9036226b3cac
Asheiou <31135263+Asheiou@users.noreply.github.com>
2025-08-17 17:33:20 +0100

    soft refactor

--- src/main/kotlin/xyz/aeolia/lib/manager/UserMapManager.kt
+++ src/main/kotlin/xyz/aeolia/lib/manager/UserMapManager.kt
@@ -72,10 +72,8 @@ object UserMapManager {
 
   @JvmStatic
   fun saveUserMap() {
-    if (userMap.isEmpty()) {
-      return  // Prevent userMap from overwriting if the plugin crashes on startup
-    }
-    if (!modified) return // No point saving if nothing's changed
+    if (userMap.isEmpty()) return  // Prevent userMap from overwriting if the plugin crashes on startup
+    if (!modified) return
     CompletableFuture.runAsync {
       try {
         FileWriter(filePath).use { writer ->
@@ -84,11 +82,12 @@ object UserMapManager {
       } catch (e: IOException) {
         throw CompletionException(e)
       }
+
     }.thenRun {
       plugin.logger.info("Saved users.json with " + userMap.size + " values!")
       modified = false
-    }
-      .exceptionally(Function { t: Throwable ->
+
+    }.exceptionally(Function { t: Throwable ->
         plugin.logger.severe("Failed to save users.json! Dumping it to console. Reason: " + t.message)
         plugin.logger.severe("StackTrace: " + t.stackTrace.contentToString())
         plugin.logger.severe("users.json: " + gson.toJson(userMap))
