41ace7f86cc7674a55f24bf3cd90c643ff5bfa2b
Asheiou <31135263+Asheiou@users.noreply.github.com>
2025-08-17 19:38:37 +0100

    cleanup

--- src/main/kotlin/xyz/aeolia/lib/manager/PermissionManager.kt
+++ src/main/kotlin/xyz/aeolia/lib/manager/PermissionManager.kt
@@ -5,17 +5,15 @@ import net.luckperms.api.model.user.User
 import net.luckperms.api.node.Node
 import net.luckperms.api.node.types.InheritanceNode
 import org.bukkit.Bukkit
-import org.bukkit.plugin.RegisteredServiceProvider
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.*
 import java.util.function.Consumer
 
 object PermissionManager {
-  var api: LuckPerms? = null
+  lateinit var api: LuckPerms
 
   @JvmStatic
   fun groupUpdate(plugin: JavaPlugin, uuid: UUID, groupname: String, status: Boolean): Boolean {
-    val api = api!!
     val userManager = api.userManager
     val userFuture = userManager.loadUser(uuid)
 
@@ -23,8 +21,7 @@ object PermissionManager {
       if (status)
         user.data().add(Node.builder("group.$groupname").build())
       else {
-        val group = api.groupManager.getGroup(groupname)
-        if (group == null) {
+        val group = api.groupManager.getGroup(groupname) ?: run {
           plugin.logger.warning("Group $groupname not found when trying to remove permission")
           return@async
         }
@@ -38,7 +35,7 @@ object PermissionManager {
 
   @JvmStatic
   fun permissionUpdate(uuid: UUID, permission: String, status: Boolean): Boolean {
-    val userManager = api!!.userManager
+    val userManager = api.userManager
     val userFuture = userManager.loadUser(uuid)
 
     userFuture.thenAcceptAsync(Consumer { user: User ->
