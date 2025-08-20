5732665b7845fd4a8056231daacdef5249d182e8
Asheiou <31135263+Asheiou@users.noreply.github.com>
2025-08-20 21:34:14 +0200

    swap order and make it all kotlinlike

--- src/main/kotlin/xyz/aeolia/lib/listener/ChatListener.kt
+++ src/main/kotlin/xyz/aeolia/lib/listener/ChatListener.kt
@@ -7,29 +7,25 @@ import org.bukkit.event.Listener
 import xyz.aeolia.lib.manager.StatusManager
 import xyz.aeolia.lib.plaintext
 import xyz.aeolia.lib.sender.MessageSender
+import xyz.aeolia.lib.utils.Message
 
 object ChatListener : Listener {
   @EventHandler(priority = EventPriority.HIGH)
   fun onChat(event: AsyncChatEvent) {
-    val plaintext = event.message().plaintext()
-    if (matcher(plaintext)) {
-      MessageSender.sendMessage(event.player, "To use commands, type /<command>.", true)
-      event.isCancelled = true
-      return
-    }
     if (StatusManager.getStatus("lockchat") && !event.player.hasPermission("lib.lockchat.exempt")) {
       event.isCancelled = true
-      MessageSender.sendMessage(event.player, "Chat has been locked by a moderator.", true)
+      MessageSender.sendMessage(event.player, Message.Chat.LOCKED, true)
+      return
+    }
+    if (matcher(event.message().plaintext())) {
+      MessageSender.sendMessage(event.player, "To use commands, type /<command>.", true)
+      event.isCancelled = true
     }
   }
 
   fun matcher(text: String): Boolean {
-    if (text[0] != ':') return false
-    var previousChar: Char? = null
-    val text = text.toMutableList()
-    text.removeAt(0)
-    if (text.isEmpty()) return false
-    text.forEach { c -> previousChar?.let { if (it == c) return false }; previousChar = c }
-    return true
+    if (!text.startsWith(":")) return false
+    val text = text.drop(1)
+    return text.zipWithNext().all { (a, b) -> a != b }
   }
 }
