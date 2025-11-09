655e67ab509baba1da4bb37c76f38f849182f581
Asheiou <31135263+Asheiou@users.noreply.github.com>
2025-11-09 20:49:26 +0000

    fix equals and hashcode

--- src/main/kotlin/xyz/aeolia/lib/data/LibBlock.kt
+++ src/main/kotlin/xyz/aeolia/lib/data/LibBlock.kt
@@ -11,4 +11,19 @@ class LibBlock(val world: String, val x: Int, val y: Int, val z: Int, val placer
 
   val block: Block
     get() = Bukkit.getWorld(world)!!.getBlockAt(x, y, z)
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other !is LibBlock) return false
+    val that = other
+    return x == that.x && y == that.y && z == that.z && world == that.world
+  }
+
+  override fun hashCode(): Int {
+    var h = world.hashCode()
+    h = 31 * h + x
+    h = 31 * h + y
+    h = 31 * h + z
+    return h
+  }
 }
